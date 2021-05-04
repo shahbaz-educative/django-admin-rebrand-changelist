650fc9056723d172cbb31d2e1a020c708f42496f
shahbaz-educative <79093482+shahbaz-educative@users.noreply.github.com>
2021-05-04 12:46:40 +0500

    removing export_to_csv

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -169,30 +169,6 @@ class QuestionAdmin(admin.ModelAdmin):
 
     make_published.short_description = "Mark selected questions as published"
 
-    def export_to_csv(modeladmin, request, queryset):
-        opts = modeladmin.model._meta
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; \filename={}.csv'.format(opts.verbose_name)
-        writer = csv.writer(response)
-        fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
-        
-        # Write a first row with header information
-        writer.writerow([field.verbose_name for field in fields])
-    
-        # Write data rows
-        for obj in queryset:
-            data_row = []
-            for field in fields:
-                value = getattr(obj, field.name)
-                if isinstance(value, datetime):
-                    value = value.strftime('%d/%m/%Y %H:%M')
-                data_row.append(value)
-            writer.writerow(data_row)
-    
-        return response
-    
-    export_to_csv.short_description = 'Export to CSV'
-
 
     def make_published_custom(self, request, queryset):
         if 'apply' in request.POST:
@@ -209,7 +185,7 @@ class QuestionAdmin(admin.ModelAdmin):
         
         return render(request, 'admin/custom_makepublished.html', context={'questions':queryset})
 
-    actions = [make_published, export_to_csv, make_published_custom]
+    actions = [make_published, make_published_custom]
 
 
     # @mark_safe
